509a2873e7d2ee3d5eec02d226848d2382d55a14
Arbel Arad <arbel@spacetime.technology>
2022-05-12 15:00:42 +0300

    added comments

--- SA-342-VCB-buttonbox.ino
+++ SA-342-VCB-buttonbox.ino
@@ -15,57 +15,51 @@ const int rocker_pins[11] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
 int stick_pos[2];
 bool rocker_pos[11] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
 int enc_pos[2] = { 512, 512 };
-Encoder enc_1(22, 21);
+Encoder enc_1(22, 21);                                      //add encoder objects
 Encoder enc_2(20, 19);
 
 void setup(){
-  analogReadResolution(12);
-  Serial.begin(115200);
+  analogReadResolution(12);                                 //analog precision set to 12bits
+  //Serial.begin(115200);
   for (int i = 0; i < 11; i++){
-    pinMode(rocker_pins[i], INPUT_PULLUP);
+    pinMode(rocker_pins[i], INPUT_PULLUP);                  //set all toggle switch pins to input_pullup
   }
 
   for (int i = 0; i < 2; i++){
-    pinMode(stick_pins[i], INPUT);
+    pinMode(stick_pins[i], INPUT);                          //set all analog pins to input - not necessary.
   }
 }
 
 void loop(){
   for (int i = 0; i < 2; i++){
-    stick_pos[i] = analogRead(stick_pins[i]);
-    Serial.print(stick_pos[i]);
-    Serial.print("\t");
+    stick_pos[i] = analogRead(stick_pins[i]);               //read stick position
   }
-  Joystick.X(map(stick_pos[0], 0, 4096, 1024, 0));
+  Joystick.X(map(stick_pos[0], 0, 4096, 1024, 0));          //send the stick position
   Joystick.Y(map(stick_pos[1], 0, 4096, 1024, 0));
 
   for (int i = 0; i < 11; i++){
-    rocker_pos[i] = !digitalRead(rocker_pins[i]);
-    Serial.print(rocker_pos[i]);
-    Serial.print("\t");
+    rocker_pos[i] = !digitalRead(rocker_pins[i]);           //read toggle switch positions
     int button_num = i;
     button_num++;
-    Joystick.button(button_num, rocker_pos[i]);
+    Joystick.button(button_num, rocker_pos[i]);             //send the button states
   }
-  Serial.println();
-  delay(100);
+  delay(10);
 
-  int enc_1_pos = 7 * enc_1.read();
+  int enc_1_pos = 7 * enc_1.read();                         //read encoder status
   int enc_2_pos = 7 * enc_2.read();
 
   if (enc_1_pos != enc_pos[0]){
-    enc_pos[0] = enc_pos[0] + enc_1_pos;
-    if (enc_pos[0] > 1024){ enc_pos[0] = 1024; }
+    enc_pos[0] = enc_pos[0] + enc_1_pos;                    //check for change in position
+    if (enc_pos[0] > 1024){ enc_pos[0] = 1024; }            //keep the encoder value in range
     if (enc_pos[0] < 0){ enc_pos[0] = 0; }
-    Joystick.sliderLeft(enc_pos[0]);
-    enc_1.write(0);
+    Joystick.sliderLeft(enc_pos[0]);                        //send the encoder position
+    enc_1.write(0);                                         //clear the encoder state
   }
   if (enc_2_pos != enc_pos[1]){
-    enc_pos[1] = enc_pos[1] + enc_2_pos;
+    enc_pos[1] = enc_pos[1] + enc_2_pos;                    //same as encoder 1
     if (enc_pos[1] > 1024){ enc_pos[1] = 1024; }
     if (enc_pos[1] < 0){ enc_pos[1] = 0; }
     Joystick.sliderRight(enc_pos[1]);
     enc_2.write(0);
   }
-
 }
